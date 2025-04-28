4575a20d674ee2bdbc7aaa9d0977e7914aca4b33
Billy Hancock <thatdevwedonttalkabout@gmail.com>
2025-04-29 11:21:31 +1200

    Created PizzaBase class with all foreseeable necessary properties. Excluded PizzaChefName method for now as that is a later task.

---
+++ Models/PizzaBase.cs
@@ -0,0 +1,17 @@
+﻿namespace PizzaSelector.Models
+{
+    public abstract class PizzaBase
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+        public bool IsGlutenFree { get; set; }
+        public bool IsVegetarian { get; set; }
+
+        public List<string> Ingredients { get; set; }
+        public List<PizzaPrice> Prices { get; set; }
+
+        public string PizzaChef { get; set; }
+
+    }
+}
---
+++ Models/PizzaPrice.cs
@@ -0,0 +1,8 @@
+﻿namespace PizzaSelector.Models
+{
+    public class PizzaPrice
+    {
+        public string Size { get; set; }
+        public float Price { get; set; }
+    }
+}
